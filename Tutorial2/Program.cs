5058723ca506aa788a56edc25317f95b648513c6
jsva4127@yahoo.co.jp <kinase@abby.co.jp>
2023-07-26 09:37:48 +0900

    learning 26-07-2023

--- Tutorial2/Program.cs
+++ Tutorial2/Program.cs
@@ -80,16 +80,57 @@ namespace HelloWorld
 
             // Commnet
 
-            // User Input
-            Console.WriteLine("Enter your age");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your age is:" + age);
-
             // My work
             // WriteLine=コンソールに文字を出力するメソッド、WriteLine（）の括弧の中身は引数
             // セミコロンをステートメントということもあります。
             //　プログラムで重要なのはデバック、console.logをフロントで見るのと同じ容量。
             Console.WriteLine("Hello World!");
+
+
+            // 定数 １とか２とか
+            // 変数　aとかxとか
+
+                // 文字列型
+                string newName; // 変数宣言, camelCaseで記載
+                newName = "kazusa"; //　値を代入
+                Console.WriteLine(newName);
+
+                newName = "Ken";
+                Console.WriteLine(newName); // 上書きする感じ
+
+                // データ型
+
+                // 整数型
+                int x = 1;
+                Console.WriteLine(x);
+                //　これより上記のあたいの渡し方の方がいい。
+                int k;
+                k = 12;
+                Console.WriteLine(k);
+
+                // 浮動小数点型(最後にmをつけるルール）
+                decimal y = 1.23m;
+                Console.WriteLine(y);
+
+                // 文字型
+                char c = 'a';
+                Console.WriteLine(c);
+
+                // 文字型
+                string s = "aiueo";
+                Console.WriteLine(s);
+
+                //論理型 boolean 大文字で出力される。
+                bool is_ok = true;
+                Console.WriteLine(is_ok);
+
+
+
+                // User Input
+                Console.WriteLine("Enter your age");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Your age is:" + age);
+
         }
     }
 }
