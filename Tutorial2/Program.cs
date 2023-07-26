8613a78ea1b4f16058da4a8f7ab3449479f9f5f2
jsva4127@yahoo.co.jp <kinase@abby.co.jp>
2023-07-26 14:27:12 +0900

    add system.Randomメソッド

--- Tutorial2/Program.cs
+++ Tutorial2/Program.cs
@@ -215,6 +215,34 @@ World!!!!
             Console.WriteLine(++x2);
             Console.WriteLine(++x2);
 
+            // ⑩ライブラリ マイクロソフトが用意した標準ライブラリ
+            // .Netクラスライブラリの標準ライブラリを使用。System.Rondomクラス
+            //インスタンス生成 diceという変数にオブジェクトの参照を格納、物を意識する.
+            //変数を１つのサイコロとしてみましょうって感じ。
+            // インスタンスを生成
+            Random dice = new Random();
+            // Nextはの Randomのメソッド diceオブジェクトの1~6までの何かの数字を出してくれるかんじ
+            //まず、`Random dice = new Random(); により、Random` クラスの新しいインスタンス dice を初期化しています。
+            //そして、`int roll = dice.Next(1, 7); で、Next(int minValue, int maxValue)`
+            //メソッドを用いて、1から6までの整数のうちの任意の一つを生成し、その値を roll に代入しています。
+            //この Next(int minValue, int maxValue) メソッドでは、最小値は含まれ、最大値は排除されるため、
+            //この例では1から6までの値が得られます。つまり、サイコロを振ったときの結果を模倣しています。
+
+            // Nextメソッドはランダムな数値を取り出すメソッドです。
+            int roll = dice.Next(1, 7);
+            // Consoleというclassの中のWriteLineというメソッドを使用してる認識
+            Console.WriteLine(roll);
+
+            roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+            roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+            roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+            roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+            roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
 
         }
     }
